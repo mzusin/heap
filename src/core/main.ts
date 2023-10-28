b62e5db2f3b12757daa38f75696c8b1056f5f6ae
Miriam Zusin <miriamzusin@gmail.com>
2023-10-28 14:46:50 +0300

    findKthGreatest()

--- src/core/main.ts
+++ src/core/main.ts
@@ -13,3 +13,23 @@ export const maxHeap = (values?: number[]) : IHeap => {
 export const minHeap = (values?: number[]) : IHeap => {
     return heap(HeapType.MinHeap, values);
 };
+
+/**
+ * Naive approach:
+ * We can extract the maximum element from the max-heap k times
+ * and the last element extracted will be the kth greatest element.
+ * Each deletion operations takes O(log n) time,
+ * so the total time complexity of this approach comes out to be O(k * log n).
+ */
+export const findKthGreatest = (values: number[], k: number): number|null => {
+    if(k < 0) return null;
+
+    const heap = maxHeap(values);
+
+    for (let i = 0; i < k - 1; i++) {
+        const topElement = heap.poll();
+        if (topElement === null) break;
+    }
+
+    return heap.peek();
+};
--- test/k-largets.test.ts
+++ test/k-largets.test.ts
@@ -1,5 +1,56 @@
-/*import { maxHeap, minHeap } from '../src/core/main';
+import { findKthGreatest } from '../src/core/main';
 
+describe('findKthGreatest()', () => {
+    it('findKthGreatest([5, 3, 8, 2, 7], 3)', () => {
+        expect(findKthGreatest([5, 3, 8, 2, 7], 3)).toEqual(5);
+    });
+
+    it('findKthGreatest([], 3)', () => {
+        expect(findKthGreatest([], 3)).toEqual(null);
+    });
+
+    it('findKthGreatest([5, 3, 8, 2, 7], 1000)', () => {
+        expect(findKthGreatest([5, 3, 8, 2, 7], 1000)).toEqual(null);
+    });
+
+    it('findKthGreatest([5, 3, 8, 2, 7], 1)', () => {
+        expect(findKthGreatest([5, 3, 8, 2, 7], 1)).toEqual(8);
+    });
+
+    it('should find the Kth largest element with negative numbers', () => {
+        const values = [5, -3, 8, -2, 7, -1];
+
+        // Find the 1st largest element.
+        expect(findKthGreatest(values, 1)).toBe(8);
+
+        // Find the 2nd largest element.
+        expect(findKthGreatest(values, 2)).toBe(7);
+
+        // Find the 3rd largest element.
+        expect(findKthGreatest(values, 3)).toBe(5);
+
+        // Find the 4th largest element.
+        expect(findKthGreatest(values, 4)).toBe(-1);
+
+        // Find the 5th largest element.
+        expect(findKthGreatest(values, 5)).toBe(-2);
+
+        // Find the 6th largest element (minimum in the original array).
+        expect(findKthGreatest(values, 6)).toBe(-3);
+    });
+
+    it('should return null for out-of-bounds K with negative numbers', () => {
+        const values = [5, -3, 8, -2, 7, -1];
+
+        // K is less than 1.
+        expect(findKthGreatest(values, -1)).toBeNull();
+
+        // K is greater than the array size.
+        expect(findKthGreatest(values, 7)).toBeNull();
+    });
+});
+
+/*
 describe('Max Heap', () => {
     it('should find the top K = 3 largest elements', () => {
         const heap = maxHeap([5, 3, 8, 2, 7], 3);
